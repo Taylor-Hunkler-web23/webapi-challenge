2fd1aae1fd3d50da8586112f4ed99dd443d849fe
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-01 11:38:48 -0400

    Created GET route to get action with specific id

--- routes/actionsRouter.js
+++ routes/actionsRouter.js
@@ -17,6 +17,39 @@ router.get('/', (req, res) => {
 });
 
 
+//returns action with specified id
+router.get('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    actiondb.get(id)
+        .then(action => {
 
+            res.status(200).json(action);
+
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: 'The action information could not be retrieved'
+            })
+        })
+
+
+});
+
+// //custom middleware
+
+function validateUserId(req, res, next) {
+    const { id } = req.params;
+    actiondb.get(id)
+        .then(user => {
+            if (user) {
+
+                next()
+            } else {
+                res.status(404).json({ message: "invalid user id" })
+            }
+        })
+
+};
 
 module.exports = router;
